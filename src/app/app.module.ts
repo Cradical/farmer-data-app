7527cc6e1d0f43be3923708f0d46811c5aad0725
Conrad Wright <easp.conrad@gmail.com>
2019-10-02 12:51:38 -0600

    Initial app setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FarmerSelectionListComponent } from './farmer-selection-list/farmer-selection-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FarmerSelectionListComponent
   ],
   imports: [
     BrowserModule,
