b5c995a39599e18adff41d14a508f266bcd879c3
Conrad Wright <easp.conrad@gmail.com>
2019-10-03 10:22:46 -0600

    Add request button to data-request compoenent

--- src/app/data-request/data-request.component.ts
+++ src/app/data-request/data-request.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-data-request',
   templateUrl: './data-request.component.html',
-  styleUrls: ['./data-request.component.css']
+  styleUrls: ['./data-request.component.css'],
 })
 export class DataRequestComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  onClick(event: { target: any }) {
+    console.log('click: ', event.target)
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
